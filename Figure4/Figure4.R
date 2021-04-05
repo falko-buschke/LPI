93b10b55e7e5fb471f57660383ee81750986c4d2
falko-buschke <falko-buschke@users.noreply.github.com>
2021-04-05 16:47:31 +0200

    Update and rename Figure5/Figure5.R to Figure4/Figure4.R

--- Figure4/Figure4.R
+++ Figure4/Figure4.R
@@ -10,7 +10,7 @@ library(vioplot)
 
 # Load the data for the empirical and reshuffled global LPI
 Global_lpi <-  read.table("LPI_global_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-Global <-  read.table("LPI_global_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+Global <-  read.table("LPI_global_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 
 #####################################
@@ -21,11 +21,11 @@ Global <-  read.table("LPI_global_null.txt",header=TRUE,sep="\t", na.strings="NA
 
 # Load the data for the empirical and reshuffled terrestrial LPI
 TR_lpi <-  read.table("LPI_terrestrial_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-TR <-  read.table("LPI_terrestrial_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+TR <-  read.table("LPI_terrestrial_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 # Load the data for the empirical and reshuffled freshwater LPI
 FW_lpi <-  read.table("LPI_freshwater_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-FW <-  read.table("LPI_freshwater_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+FW <-  read.table("LPI_freshwater_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 # Load the data for the empirical and reshuffled marine LPI
 MR_lpi <-  read.table("LPI_marine_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
@@ -40,23 +40,23 @@ MR <-  read.table("LPI_marine_null1.txt",header=TRUE,sep="\t", na.strings="NA", 
 
 # Load the data for the empirical and reshuffled Afrotropical LPI
 Afro_lpi <-  read.table("LPI_Afro_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-Afro <-  read.table("LPI_Afro_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+Afro <-  read.table("LPI_Afro_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 # Load the data for the empirical and reshuffled Nearctic LPI
 Near_lpi <-  read.table("LPI_Near_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-Near <-  read.table("LPI_Near_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+Near <-  read.table("LPI_Near_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 # Load the data for the empirical and reshuffled Neotropical LPI
 Neo_lpi <-  read.table("LPI_Neo_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-Neo <-  read.table("LPI_Neo_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+Neo <-  read.table("LPI_Neo_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 # Load the data for the empirical and reshuffled INdo-Pacific LPI
 Indo_lpi <-  read.table("LPI_Indo_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-Indo <-  read.table("LPI_Indo_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+Indo <-  read.table("LPI_Indo_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 # Load the data for the empirical and reshuffled Palearctic LPI
 Pale_lpi <-  read.table("LPI_Pale_empirical.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
-Pale <-  read.table("LPI_Pale_null.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
+Pale <-  read.table("LPI_Pale_null1.txt",header=TRUE,sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 
 #########################################################################
 
@@ -91,12 +91,17 @@ Obs <- cbind(Global_lpi$LPI_final[dim(Global_lpi)[1]],
 			Afro_lpi$LPI_final[dim(Global_lpi)[1]],
 			Indo_lpi$LPI_final[dim(Global_lpi)[1]])
 
+nulls.med <- apply(Nulls,2,median)
+emp.val <- Obs
+
+dif <- round(nulls.med-emp.val,digits=3)
+
 # Define the colour ramp
-cols <- colorRampPalette(c(rgb(0,0,0.5,1),rgb(0,0.8,0.8,1),rgb(1,0.5,0,1)),interpolate="linear")(9)
+cols <- colorRampPalette(c(rgb(0,0,0.8,1),rgb(0,0.8,0.8,1),rgb(1,0.5,0,1)),interpolate="linear")(9)
 
 
 # Set up the plot and dimensions
-png(filename="Figure5.png",width=28,height=12,units="cm",res=300)
+png(filename="Figure4.png",width=28,height=12,units="cm",res=300)
 
 # Define plot margins
 par(mai=c(0.4,0.8,0.1,0.1))
@@ -115,7 +120,6 @@ text (c(0.3,1.65,4.65),c(1.3,1.3,1.3), c("a","b","c"),cex=1.5, font=2,pos=3)
 
 # Add a legend
 legend("topright", "Empirical LPI", pch=23, col="black",bg=rgb(1,1,1,0.85), cex=1.2)
-
+text(1:9,rep(0,9),dif, col=ifelse(dif>0,"blue","red"))
 # Close plot device and save image to file
 dev.off()
-
